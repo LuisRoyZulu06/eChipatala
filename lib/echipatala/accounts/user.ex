871862d48ad66b9d80c6ca924541737b19fdf38e
unknown <lunje@probasegroup.com>
2021-10-07 14:35:48 +0200

    again

--- lib/echipatala/accounts/user.ex
+++ lib/echipatala/accounts/user.ex
@@ -53,3 +53,4 @@ defmodule Echipatala.Accounts.User do
 end
 
 #Echipatala.Accounts.create_user(%{name: "Luis Roy", username: "Luis", email: "luis@probasegroup.com", password: "password06", user_type: 1, user_role: "ADMIN", status: "1", phone: "260979797337", gender: "M", auto_password: "N",  inserted_at: NaiveDateTime.utc_now, updated_at: NaiveDateTime.utc_now})
+#Echipatala.Accounts.create_user(%{name: "Myself", username: "me", email: "lunje@probasegroup.com", password: "password06", user_type: 1, user_role: "ADMIN", status: "1", phone: "260979797337", gender: "M", auto_password: "N",  inserted_at: NaiveDateTime.utc_now, updated_at: NaiveDateTime.utc_now})
